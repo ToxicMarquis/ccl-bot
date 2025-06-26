f5172f579fccd48f8545ebe671915812552bfc80
Marquis <78649236+ToxicMarquis@users.noreply.github.com>
2025-06-26 11:53:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ if not BOT_TOKEN:
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-CHANNEL_ID = os.getenv("CHANNEL_ID", "1613317747")
+CHANNEL_ID = os.getenv("CHANNEL_ID", "2518300510")
 CHANNEL_URL = os.getenv("CHANNEL_URL", "https://t.me/chesstourname")
 TEAM_URL = "https://lichess.org/team/ilAYFF9R"
 
